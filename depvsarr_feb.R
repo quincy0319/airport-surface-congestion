e8bb2447e99bbe3abb16016d117fca4c8809fb55
quincy0319 <quincy0319@gmail.com>
2016-09-07 11:13:51 +0800

    0907

--- depvsarr_feb.R
+++ depvsarr_feb.R
@@ -1,11 +1,11 @@
-x <- read.csv("rwy_dep_feb.csv", header = F)
+x <- read.csv("dep_feb_per60.csv", header = F)
 x01 <- x[, 1]
 x18l <- x[, 2]
 x18r <- x[, 3]
 x19 <- x[, 4]
 x36l <- x[, 5]
 x36r <- x[, 6]
-y <- read.csv("rwy_arr_feb.csv", header = F)
+y <- read.csv("arr_feb_per60.csv", header = F)
 y01 <- y[, 1]
 y18l <- y[, 2]
 y18r <- y[, 3]
@@ -14,7 +14,7 @@ y36l <- y[, 5]
 y36r <- y[, 6]
 
 opar <- par(no.readonly = TRUE)
-par(mfrow = c(3, 2))
+par(mfrow = c(2, 3))
 smoothScatter(x01, y01, xlab = "dep_01_perhour", ylab = "arr_01_perhour")
 smoothScatter(x18l, y18l, xlab = "dep_18l_perhour", ylab = "arr_18l_perhour")
 smoothScatter(x18r, y18r, xlab = "dep_18r_perhour", ylab = "arr_18l_perhour")
