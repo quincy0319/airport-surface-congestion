24cc7894d1c0dfa8e7eca2d721562a899268baff
quincy0319 <quincy0319@gmail.com>
2016-10-09 15:44:39 +0800

    1009 nice try~ failed

--- taxi_time_ols_analysis.R
+++ taxi_time_ols_analysis.R
@@ -1,5 +1,29 @@
 # taxi time caculation and analysis
-source(data_source.R)
 
-attach()
-fit_36l <- lm ()
+dep_36l_feb_finished <- read.csv("dep_36l_feb_finished.csv")
+dep_36l_ols <- subset(dep_36l_feb_finished, dep_36l_feb_finished$dep_36l_in_cluster == 1,
+                      select = c(15, 21, 22))
+y <- as.numeric(dep_36l_ols$dep_taxi)
+x1 <- dep_36l_ols$dep_36l_per_dep
+x2 <- dep_36l_ols$arr_36l_per_dep
+fit_36l_dep <- lm (y ~ x1)
+summary(fit_36l_dep)
+
+dep_36r_feb_finished <- read.csv("dep_36r_feb_finished.csv")
+dep_36r_ols <- subset(dep_36r_feb_finished, dep_36r_feb_finished$dep_36r_in_cluster == 1,
+                      select = c(15, 21, 22))
+y <- as.numeric(dep_36r_ols$dep_taxi)
+x1 <- dep_36r_ols$dep_36r_per_dep
+x2 <- dep_36r_ols$arr_36r_per_dep
+fit_36r_dep <- lm (y ~ x1 + x2)
+summary(fit_36r_dep)
+
+dep_01_feb_finished <- read.csv("dep_01_feb_finished.csv")
+dep_01_ols <- subset(dep_01_feb_finished, dep_01_feb_finished$dep_01_in_cluster == 1,
+                     select = c(15, 21, 22))
+y <- as.numeric(dep_01_ols$dep_taxi)
+x1 <- dep_01_ols$dep_01_per_dep
+x2 <- dep_01_ols$arr_01_per_dep
+fit_01_dep <- lm(y ~ x1 + x2)
+summary(fit_01_dep)
+
