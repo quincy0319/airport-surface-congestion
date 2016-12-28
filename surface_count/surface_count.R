8b9e9a01d11e420b0ea400581f54e5f0fff9635f
quincy0319 <quincy0319@gmail.com>
2016-12-28 23:30:23 +0800

    1228 unable to fix env error, sad

--- surface_count/surface_count.R
+++ surface_count/surface_count.R
@@ -10,7 +10,7 @@ dep_count <- window_count_feb2may[, 1]
 arr_count <- window_count_feb2may[, 2]
 pb_count <- window_count_feb2may[, 3]
 serial_num <- seq(from = 1, to = 11520, by = 1)
-# surface count vs time plot
+# data preparation
 day1 <- rep(c("day1"), times = 96)
 day2 <- rep(c("day2"), times = 96)
 day3 <- rep(c("day3"), times = 96)
@@ -19,13 +19,31 @@ window_day1 <- seq(from = 1, to = 96, by = 1)
 window_day2 <- seq(from = 1, to = 96, by = 1)
 window_day3 <- seq(from = 1, to = 96, by = 1)
 window_day4 <- seq(from = 1, to = 96, by = 1)
-surface_count_per_window <- surface_count[1:(96 * 4)]
+surface_count_per_window <- surface_count[c(1:96, (96 * 30 + 1):(96 * 31), 
+					(96 * 66 + 1):(96 * 67),
+					(96 * 99 + 1):(96 * 100))]
 day1_4 <- c(day1, day2, day3, day4)
 window1_4 <- c(window_day1, window_day2, window_day3, window_day3)
 surface_count_plot_day1_4 <- data.frame(window1_4, 
 				surface_count_per_window, day1_4)
+# time window ~ surface count plot
 library(ggplot2)
 plot1 <- ggplot(surface_count_plot_day1_4, 
 		aes(x = window1_4, y = surface_count_per_window, 
-			colour = day1_4))
-plot1 + geom_line()
+			linetype = day1_4, colour = day1_4))
+plot1 + geom_line(size = 1.1, alpha = .8) +
+labs(x = "时刻", y = "场面航班数/15分钟", title = "场面航班数变化图", size = 2) +
+scale_x_continuous(limits = c(0, 96), breaks = c(0, 24, 48, 72, 96),
+		labels = c("0000", "0600", "1200", "1800", "2400"))
+
+# max surface count
+surface_count_max <- data.matrix(read.csv("surface_count_max.csv", header = FALSE))
+serial_num <- seq(from = 1, to = 120, by = 1)
+surface_count_analysis <- data.frame(serial_num, surface_count_max)
+names(surface_count_analysis) <- c("serial_num", "surface_count_max")
+library(ggplot2)
+plot2 <- ggplot(surface_count_analysis, 
+		aes(x = surface_count_max, y = ..density..)) + geom_point()
+plot2 + geom_histogram() 
+scale_x_log10() +
+scale_y_log10()
