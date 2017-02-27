85ab3c58d3dff543526c3aad8f4e4a9a521ccc44
quincy0319 <quincy0319@gmail.com>
2017-02-27 21:11:22 +0800

    0227 diffent arr

--- operational_throughput_envelope/average_departure_throughput.R
+++ operational_throughput_envelope/average_departure_throughput.R
@@ -1,5 +1,5 @@
 setwd("C:/Users/QYF/Documents/Visual Studio 2015/Projects/airport_congestion/operational_throughput_envelope")
-# paper chapter 2
+# paper chapter 4
 # dep_rate~dep_demand关系图、误差线、拟合曲线
 source("deal_with_data.R")
 library(ggplot2)
@@ -75,3 +75,90 @@ fit_median <- lm(demand_median ~ dep_demand + I(dep_demand ^ 2))
 summary(fit_median)
 
 qqPlot(fit_mean, id.method = "identify", simulate = TRUE, main = "qqplot")
+
+###############################################################################
+# dep_rate ~ (dep_demand, arr_rate)
+
+# read origin data
+window_count_per15 <- read.csv("window_count_feb2may.csv")
+# 不同arr下的子集，arr最大值为25
+# 用循环的方式，
+
+window_sub_0arr <- subset(window_count_per15, arr_count_per15 == 0)
+
+# 0arr
+demand_max_per15_0arr <- max(window_sub_0arr$dep_demand_per15)
+dep_max_per15_0arr <- max(window_sub_0arr$dep_count_per15)
+arr_max_per15_0arr <- max(window_sub_0arr$arr_count_per15)
+# 做曲线前准备的数据
+dep_mean_0arr <- vector(mode = "numeric", length = demand_max_per15_0arr)
+dep_median_0arr <- vector(mode = "numeric", length = demand_max_per15_0arr)
+dep_max_0arr <- vector(mode = "numeric", length = demand_max_per15_0arr)
+dep_min_0arr <- vector(mode = "numeric", length = demand_max_per15_0arr)
+dep_sd_0arr <- vector(mode = "numeric", length = demand_max_per15_0arr)
+for (i in 1:demand_max_per15_0arr) {
+	dep_mean_0arr[i] <- mean(as.matrix(subset(window_sub_0arr,
+		dep_demand_per15 == i, select = 1)))
+	dep_median_0arr[i] <- median(as.matrix(subset(window_sub_0arr,
+		dep_demand_per15 == i, select = 1)))
+	dep_max_0arr[i] <- max(as.matrix(subset(window_sub_0arr,
+		dep_demand_per15 == i, select = 1)))
+	dep_min_0arr[i] <- min(as.matrix(subset(window_sub_0arr,
+		dep_demand_per15 == i, select = 1)))
+	dep_sd_0arr[i] <- sd(as.matrix(subset(window_sub_0arr,
+		dep_demand_per15 == i, select = 1)))
+}
+# 做点图及误差线
+x <- c(1:61)
+# 同一推出率下起飞率的平均数、中位数、标准差
+# 只取推出1至24架的情况
+y1 <- dep_mean_0arr[c(1:61)]
+# 去除y1中的na值，同时去除相应的x值
+x_y1 <- x[-which(is.na(y1))]
+y1 <- y1[-which(is.na(y1))]
+y2 <- dep_median_0arr[c(1:61)]
+err_sd <- dep_sd_0arr[c(1:61)]
+# 做图(平均数图）
+# 平均数（实心三角）
+plot(x_y1, y1, lty = 1, cex = 1)
+################################################################################
+# 回归分析
+# 2次拟合
+fit_mean_2_0arr <- lm(y1 ~ x_y1 + I(x_y1 ^ 2))
+lines(x_y1, fitted(fit_mean_2_0arr))
+
+################################################################################
+# 1-25 arr
+for (j in 1:25) {
+	window_sub <- subset(window_count_per15, arr_count_per15 == j)
+	demand_max_per15 <- max(window_sub$dep_demand_per15)
+	dep_max_per15 <- max(window_sub$dep_count_per15)
+	arr_max_per15 <- max(window_sub$arr_count_per15)
+	dep_mean <- vector(mode = "numeric", length = demand_max_per15)
+	dep_median <- vector(mode = "numeric", length = demand_max_per15)
+	dep_max <- vector(mode = "numeric", length = demand_max_per15)
+	dep_min <- vector(mode = "numeric", length = demand_max_per15)
+	dep_sd <- vector(mode = "numeric", length = demand_max_per15)
+	for (i in 1:demand_max_per15) {
+		dep_mean[i] <- mean(as.matrix(subset(window_sub,
+			dep_demand_per15 == i, select = 1)))
+		dep_median[i] <- median(as.matrix(subset(window_sub,
+			dep_demand_per15 == i, select = 1)))
+		dep_max[i] <- max(as.matrix(subset(window_sub,
+			dep_demand_per15 == i, select = 1)))
+		dep_min[i] <- min(as.matrix(subset(window_sub,
+			dep_demand_per15 == i, select = 1)))
+		dep_sd[i] <- sd(as.matrix(subset(window_sub,
+			dep_demand_per15 == i, select = 1)))
+	}
+	x <- c(1:length(dep_mean))
+	# 同一推出率下起飞率的平均数、中位数、标准差
+	y_mean <- floor(dep_mean[c(1:length(dep_mean))])
+	x <- x[is.na(y_mean) == F]
+	y_mean <- na.omit(y_mean)
+	################################################################################
+	# 回归分析
+	# 2次
+	fit_mean_2 <- lm(y_mean ~ x + I(x ^ 2))
+	lines(x, fitted(fit_mean_2))
+}
