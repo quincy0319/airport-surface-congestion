62904f147b6e788fbd4ff4b7ea295398e03e774b
quincy0319 <quincy0319@gmail.com>
2017-02-28 22:56:31 +0800

    0228 midnight finish adjusted traffic pic

--- operational_throughput_envelope/average_departure_throughput.R
+++ operational_throughput_envelope/average_departure_throughput.R
@@ -258,7 +258,8 @@ for (i in 1:2511) {
 }
 ote <- data.frame(ote, ote_weight)
 # ote linear regression
-ote_lm <- lm(dep_count_per15 ~ arr_count_per15 + I(arr_count_per15 ^ 2), data = ote)
+ote_lm <- lm(dep_count_per15 ~ arr_count_per15 + I(arr_count_per15 ^ 2),
+	data = ote)
 xmin <- min(ote$arr_count_per15)
 xmax <- max(ote$arr_count_per15)
 predicted <- data.frame(arr_count_per15 = seq(xmin, xmax, length.out = 2511))
@@ -277,17 +278,77 @@ labs(size = "数据频次")
 
 setwd("C:/Users/QYF/Documents/Visual Studio 2015/Projects/airport_congestion/operational_throughput_envelope")
 # paper chapter 5
+# 给航班数据上加上demand值
 dep_processed <- read.csv("dep_processed_feb2may.csv")
+window_count_per15 <- read.csv("window_count_feb2may.csv")
+dep_demand <- vector(mode = "numeric", length = nrow(dep_processed))
+for (j in 1:nrow(dep_processed)) {
+	window_demand <- (dep_processed$day_of_case[j] - 1) * 96 +
+	dep_processed$window_of_day[j]
+	dep_demand[j] <- window_count_per15$dep_demand_per15[window_demand]
+}
+dep_processed <- data.frame(dep_processed, dep_demand)
+
+# 将demand加上推出航班数得到修正场面航班数 
 adjusted_traffic <- vector(mode = "numeric", length = nrow(dep_processed))
 for (i in 1:nrow(dep_processed)) {
 	date_of_i <- dep_processed$mission_full_date[i]
-	dep_demand_of_i <- dep_processed$dep_15_per_dep[i]
+	dep_demand_of_i <- dep_processed$dep_demand[i]
 	t_min <- dep_processed$pb_time[i]
 	t_max <- dep_processed$time_takeoff_real[i]
 	dep_no_midnight <- subset(dep_processed, pb_time <= time_takeoff_real)
 	dep_sameday <- subset(dep_no_midnight, mission_full_date == date_of_i)
- 	dep_sameday_pb <- subset(dep_sameday, pb_time >= t_min)
+	dep_sameday_pb <- subset(dep_sameday, pb_time >= t_min)
 	dep_sameday_pb_to <- subset(dep_sameday, pb_time <= t_max)
 	nadj <- nrow(dep_sameday_pb_to) + dep_demand_of_i
 	adjusted_traffic[i] <- nadj
 }
+
+adjusted_traffic <- read.csv("adjusted_traffic.csv")
+
+# plot adj_traffic vs dep_taxi
+dep_taxi <- dep_processed$dep_taxi
+adj_vs_taxi <- data.frame(dep_taxi, adjusted_traffic)
+# 作图前准备
+adj_max <- max(adjusted_traffic)
+taxi_mean <- vector(mode = "numeric", length = adj_max)
+taxi_median <- vector(mode = "numeric", length = adj_max)
+taxi_max <- vector(mode = "numeric", length = adj_max)
+taxi_min <- vector(mode = "numeric", length = adj_max)
+taxi_sd <- vector(mode = "numeric", length = adj_max)
+
+for (i in 1:adj_max) {
+	taxi_mean[i] <- mean(as.matrix(subset(adj_vs_taxi,
+		adjusted_traffic == i, select = 1)))
+	taxi_median[i] <- median(as.matrix(subset(adj_vs_taxi,
+		adjusted_traffic == i, select = 1)))
+	taxi_max[i] <- max(as.matrix(subset(adj_vs_taxi,
+		adjusted_traffic == i, select = 1)))
+	taxi_min[i] <- min(as.matrix(subset(adj_vs_taxi,
+		adjusted_traffic == i, select = 1)))
+	taxi_sd[i] <- sd(as.matrix(subset(adj_vs_taxi,
+		adjusted_traffic == i, select = 1)))
+}
+adj_taxi_num <- c(1:max(adj_max))
+adj_statistical_data <- data.frame(taxi_mean, adj_taxi_num)
+
+library(ggplot2)
+plot_taxi_adj <- ggplot(adj_statistical_data,
+			aes(x = adj_taxi_num, y = taxi_mean))
+
+plot_output <- plot_taxi_adj +
+	geom_errorbar(data = adj_statistical_data,
+		aes(ymin = taxi_mean - taxi_sd,
+			ymax = taxi_mean + taxi_sd),
+			size = 0.9, width = .7, alpha = .5) +
+	geom_point(size = 3.5, alpha = .8) +
+	labs(x = "修正场面航班数（架次/15分钟）", y = "离场滑行时间（分钟）", size = 2) +
+	scale_x_discrete(limits = c(0, 60)) +
+	scale_x_continuous(limits = c(0, 60), breaks = c(0, 5, 10, 15, 20, 25,
+		30, 35, 40, 45, 50, 55, 60),
+		labels = c(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60)) +
+	scale_y_discrete(limits = c(0, 40)) +
+	scale_y_continuous(limits = c(0, 40), breaks = c(0, 5, 10, 15, 20, 25,
+		30, 35, 40),
+		labels = c(0, 5, 10, 15, 20, 25, 30, 35, 40))
+
