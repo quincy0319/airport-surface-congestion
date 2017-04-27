70b924af128d522eeefb2181325d7f967c8347a0
quincy0319 <quincy0319@gmail.com>
2017-04-27 19:12:42 +0800

    0427 taxi delay caculated

--- surface_congestion_analysis_and_control/unimpeded_taxi.R
+++ surface_congestion_analysis_and_control/unimpeded_taxi.R
@@ -187,12 +187,6 @@ for (i in 1:33) {
 		(adjusted_traffic < 25) & (adjusted_traffic > 5))
 	unimpeded_taxi_time_36l[i] <- mean(adj_taxi_ramp_i_unimpeded$dep_taxi)
 }
-unimpeded_taxi <- vector(mode = "numeric", length = nrow(dep_processed))
-for (j in 1:nrow(dep_processed)) {
-	ramp_num <- dep_processed$ramp[j]
-	unimpeded_taxi[j] <- unimpeded_taxi_time[ramp_num]
-}
-dep_processed <- data.frame(dep_processed, unimpeded_taxi)
 
 ###############################################################################
 ###############################################################################
@@ -205,12 +199,6 @@ for (i in 1:33) {
 		(adjusted_traffic < 16)&(adjusted_traffic > 6))
 	unimpeded_taxi_time_36r[i] <- mean(adj_taxi_ramp_i_unimpeded$dep_taxi)
 }
-unimpeded_taxi <- vector(mode = "numeric", length = nrow(dep_processed))
-for (j in 1:nrow(dep_processed)) {
-	ramp_num <- dep_processed$ramp[j]
-	unimpeded_taxi[j] <- unimpeded_taxi_time[ramp_num]
-}
-dep_processed <- data.frame(dep_processed, unimpeded_taxi)
 
 ###############################################################################
 ###############################################################################
@@ -222,15 +210,58 @@ for (i in 1:33) {
 	adj_taxi_ramp_i_unimpeded <- subset(dep_ramp_i, adjusted_traffic < 12)
 	unimpeded_taxi_time_01[i] <- mean(adj_taxi_ramp_i_unimpeded$dep_taxi)
 }
-unimpeded_taxi <- vector(mode = "numeric", length = nrow(dep_processed))
-for (j in 1:nrow(dep_processed)) {
-	ramp_num <- dep_processed$ramp[j]
-	unimpeded_taxi[j] <- unimpeded_taxi_time[ramp_num]
-}
-dep_processed <- data.frame(dep_processed, unimpeded_taxi)
 
 
 ###############################################################################
+# 本文计算的畅通滑行时间
 unimpeded_taxi_time <- data.frame(unimpeded_taxi_time_36l,
 	unimpeded_taxi_time_36r, unimpeded_taxi_time_01)
 names(unimpeded_taxi_time) <- c("36L", "36R", "01")
+
+# 唐华龙计算的畅通滑行时间
+unimpeded_taxi_time_tang_36l <- c(NA, NA, 10.53, 12.62, NA, 10.34, NA, NA, NA,
+	10.44, NA, 12.14, 9.44, 7.72, 6.62, 5.56, 5.82, 6.94, 5.41, 6.23, 7.02,
+	6.80, 5.64, 6.26, 5.23, 4.90, 5, NA, 7.76, 11.56, NA, NA, NA)
+unimpeded_taxi_time_tang_36r <- c(4.22, 5.24, 3.67, 3.45, 4.52, 3.95, 5.7, 7.11,
+	6.16, 4.82, 8.64, 5.33, 4.19, 3.02, 3.06, 3.98, 5.08, 5.24, 7.79, 5.58,
+	4.89, 5.51, 5.87, 4.79, 5.27, 6.05, 6.31, NA, 6.47, 9.58, 7.22, NA, NA)
+unimpeded_taxi_time_tang_01 <- c(5.13, 4.7, 5.97, 5.99, 5.53, 5.63, 6.39, 6.75,
+	7.69, 6.46, 6.99, 8.23, NA, NA, NA, NA, NA, 11.79, NA, NA, 10.62, 12.01,
+	NA, NA, NA, NA, NA, NA, NA, 10.08, NA, NA, NA)
+unimpeded_taxi_time_tang <- data.frame(unimpeded_taxi_time_tang_36l,
+	unimpeded_taxi_time_tang_36r, unimpeded_taxi_time_tang_01)
+names(unimpeded_taxi_time_tang) <- c("36L", "36R", "01")
+
+###############################################################################
+# 滑行延误计算
+# 首先计算各停机位到各跑道的平均滑行时间
+average_taxi_36l <- vector(mode = "numeric", length = 33)
+for (i in 1:33) {
+	dep_ramp_i <- subset(adj_vs_taxi_36l, ramp == i)
+	average_taxi_36l[i] <- mean(dep_ramp_i$dep_taxi)
+}
+average_taxi_36r <- vector(mode = "numeric", length = 33)
+for (i in 1:33) {
+	dep_ramp_i <- subset(adj_vs_taxi_36r, ramp == i)
+	average_taxi_36r[i] <- mean(dep_ramp_i$dep_taxi)
+}
+average_taxi_01 <- vector(mode = "numeric", length = 33)
+for (i in 1:33) {
+	dep_ramp_i <- subset(adj_vs_taxi_01, ramp == i)
+	average_taxi_01[i] <- mean(dep_ramp_i$dep_taxi)
+}
+
+average_taxi <- data.frame(average_taxi_36l, average_taxi_36r, average_taxi_01)
+names(average_taxi) <- c("36L", "36R", "01")
+
+# 分别计算两种滑行延误
+taxi_delay_qu <- average_taxi - unimpeded_taxi_time
+taxi_delay_tang <- average_taxi - unimpeded_taxi_time_tang
+
+###############################################################################
+# 输出畅通滑行时间与滑行延误
+write.csv(unimpeded_taxi_time, "unimpeded_taxi_time_qu.csv", row.names = F)
+write.csv(unimpeded_taxi_time_tang, "unimpeded_taxi_time_tang.csv", row.names = F)
+write.csv(taxi_delay_qu, "taxi_delay_qu.csv", row.names = F)
+write.csv(taxi_delay_tang, "taxi_delay_tang.csv", row.names = F)
+
